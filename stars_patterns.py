5b47132ac4c75b548663595a9e7f13a984a5dcfb
XYZ <sumayyasadaf2004@gmail.com>
2026-09-03 18:33:57 +0530

    Floyed triangle created

--- stars_patterns.py
+++ stars_patterns.py
@@ -1,5 +1,15 @@
+'''
 c=input("Enter the character!")
 for i in range(1,int(input("Enter number of rows:"))+1):    
     for j in range(1,i+1):  
         print(c,end=" ")
+    print()
+'''
+
+# floyed triangle
+k=1
+for i in range(1,int(input("Enter numebr of rows:"))+1):
+    for j in range(i):
+        print(k,end=" ")
+        k+=1
     print()
