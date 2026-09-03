86cb058162805314fec5fcb440f2d5b987ce6d4c
XYZ <sumayyasadaf2004@gmail.com>
2026-09-03 18:38:24 +0530

    Floyed triangle with character A is added

--- stars_patterns.py
+++ stars_patterns.py
@@ -4,7 +4,6 @@ for i in range(1,int(input("Enter number of rows:"))+1):
     for j in range(1,i+1):  
         print(c,end=" ")
     print()
-'''
 
 # floyed triangle
 k=1
@@ -12,4 +11,15 @@ for i in range(1,int(input("Enter numebr of rows:"))+1):
     for j in range(i):
         print(k,end=" ")
         k+=1
+    print()
+
+'''
+k='A'
+for i in range(1,int(input("Enter numebr of rows:"))+1):
+    for j in range(i):
+        print(k,end=" ")
+        k=ord(k)
+        k+=1
+        k=chr(k)
+        
     print()
